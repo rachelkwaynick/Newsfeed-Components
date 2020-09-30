f6d6d4cb878b9dc61d5c124c63ccb6d13ebf5f00
Rachel Waynick <rachelkwaynick@gmail.com>
2020-09-30 13:49:42 -0700

    articles done

--- components/Article.js
+++ components/Article.js
@@ -86,7 +86,28 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
+  },
+  {
+    title: 'Lambda School Students: "We\'re the best!"',
+    date: 'Nov 5th, 2018',
+    firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+        moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
+        watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
+        Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
+        jar twi'lek jinn leia jango skywalker mon.`,
+
+    secondParagraph: `Grievous fett calamari anakin skywalker hutt.Alderaan darth kenobi darth r2- d2
+        windu mothma.Sidious darth calamari moff.Wampa mothma sith wedge solo mara.Darth gonk maul sith moff chewbacca palpatine
+        mace amidala.C - 3po solo skywalker anakin yoda leia.Maul wampa bespin watto jade ewok darth jabba.Lando dantooine moff
+        k - 3po dantooine luke.Fisto mandalore darth wedge c - 3p0 ahsoka.Secura moff palpatine fett.Anakin sith darth darth.Moff
+        solo leia ben ponda jade.Binks jango aayla skywalker skywalker cade.Mustafar darth ventress anakin watto.Yavin jawa sebulba
+        owen jinn tatooine sith organa.`,
+
+    thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+        naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
+        han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+  },
 ];
 
 /*
@@ -114,3 +135,46 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+function articleMaker(article) {
+  const art = document.createElement('div');
+  const head = document.createElement('h2');
+  const date = document.createElement('p');
+  const body1 = document.createElement('p');
+  const body2 = document.createElement('p');
+  const body3 = document.createElement('p');
+  const btn = document.createElement('span');
+
+  art.appendChild(head);
+  art.appendChild(date);
+  art.appendChild(body1);
+  art.appendChild(body2);
+  art.appendChild(body3);
+  art.appendChild(btn);
+
+  art.classList.add('article');
+  date.classList.add('date');
+  btn.classList.add('expandButton');
+
+  head.textContent = article.title;
+  date.textContent = article.date;
+  body1.textContent = article.firstParagraph;
+  body2.textContent = article.secondParagraph;
+  body3.textContent = article.thirdParagraph;
+
+  btn.textContent = '+';
+
+  btn.addEventListener('click', () => {
+    art.classList.toggle('article-open')
+  })
+
+
+  return art
+}
+
+const articles = document.querySelector('.articles')
+
+data.forEach(article => {
+  const art = articleMaker(article)
+  articles.appendChild(art)
+})
